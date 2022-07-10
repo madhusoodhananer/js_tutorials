f642b92884a0077c424ba1497aa27d6fec3088a0
Madhusoodanan <madhusoodhananer@gmail.com>
2022-07-10 19:46:52 +0530

    terinnary operator

---
+++ js/terinnary.js
@@ -0,0 +1,8 @@
+console.log('****************TERNARY OPERATOR****************');
+
+let number1 = 10;
+let number2 = 20;
+let result = number1 > number2 ? number1 : number2;
+console.log(`The greater number is ${result}`);
+
+console.log('****************TERNARY OPERATOR****************');
